b9b48cdf65ca2c371a5abe8e08d84d176dece7da
iwanggiwangkaraa <91723641+iwanggiwangkaraa@users.noreply.github.com>
2022-02-02 13:21:45 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,24 @@
+//Soal 1
+function trafficLight(lampu) {
+    switch(lampu) {
+        case "red" :
+            return 'Stop';
+            break;
+        case "yellow" :
+            return 'Be careful';
+            break;
+        case "green" :
+            return 'Go!'
+            break;
+    }
+}
+console.log(trafficLight("green"));
+
+
+
+
+//Soal 2
+const persegi = (sisi) => {
+    return sisi * sisi**2
+}
+console.log(persegi(2));
